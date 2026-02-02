5a0fe19a36ee526faa1fdb461bc2bd7bbff38d9f
aadit11 <aadit.kshirsagar@gmail.com>
2026-02-02 15:43:24 -0800

    added code

---
+++ Graphs/ini.py
@@ -0,0 +1,24 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+
+        self.graph[u].appned(v)
+        self.graph[v].append(u)
+
+    def display(self):
+        for node in self.graph:
+            print(node, "->", self.graph[node])
+
+
+g = Graph()
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 4)
+g.add_edge(3, 4)
+        
