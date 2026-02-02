7ef92805c154107fbad63bf0c5fecf4fb59b8cea
aadit11 <aadit.kshirsagar@gmail.com>
2026-02-01 19:18:58 -0800

    added code

---
+++ LinkedList/ini.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next= None
+
+node1 = Node(20)
+node2 = Node(30)
+node3 = Node(40)
+node4 = Node(50)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+head = node1
+
+current = node1
+while current:
+    print (current.data, end =  "->")
+    current = current.next
+
+print("None")
