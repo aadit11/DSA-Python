8d18a283fe7a002d18d8b4f17c9f7c7a1d82a7a4
aadit11 <aadit.kshirsagar@gmail.com>
2026-02-01 19:27:36 -0800

    added code

---
+++ LinkedList/reverse.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+node1 = Node(20)
+node2 = Node(30)
+node3 = Node(40)
+node4 = Node(50)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+head = node1
+
+current = head
+prev = None
+
+while current:
+    next_node = current.next
+    current.next = prev
+    prev = current
+    current = next_node
+
+head = prev
+
+
+current = head
+while current:
+    print (current.data, end = "->")
+    current = current.next
+
+print("None")
